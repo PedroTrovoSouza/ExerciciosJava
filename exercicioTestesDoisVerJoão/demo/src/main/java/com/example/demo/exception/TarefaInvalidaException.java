e7aeafde856dcf7e35e7b9d7df8d79e30c27e2bd
Pedro Trovo <pedro.tsouza@solutis.com.br>
2025-05-13 13:58:33 -0300

    Delete exercicioTestesDoisVerJoão directory

--- exercicioTestesDoisVerJoão/demo/src/main/java/com/example/demo/exception/TarefaInvalidaException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exception;
-
-
-public class TarefaInvalidaException extends RuntimeException {
-    public TarefaInvalidaException(String message) {
-        super(message);
-    }
-}
