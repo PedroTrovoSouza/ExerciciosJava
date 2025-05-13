50425f2214fa8e36d640d0c6b3c45444445d8291
Pedro Trovo <pedro.tsouza@solutis.com.br>
2025-05-13 13:58:17 -0300

    Delete Locadora directory

--- Locadora/src/main/java/com/br/locadora/Locadora/dto/ClienteDto.java
+++
@@ -1,9 +0,0 @@
-package com.br.locadora.Locadora.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record ClienteDto(
-        @NotBlank String nome,
-        @NotBlank String email
-) {
-}
--- Locadora/src/main/java/com/br/locadora/Locadora/dto/TesteLocacoDto.java
+++
@@ -1,7 +0,0 @@
-package com.br.locadora.Locadora.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record TesteLocacoDto(@NotBlank String cliente,
-                             @NotBlank String filme) {
-}
